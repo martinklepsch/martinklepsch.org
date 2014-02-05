37dd319c93b604450dca947741abea908848f771
Martin Klepsch <martinklepsch@gmail.com>
2014-02-05 23:57:50 +0100

    change link scheme again :D

--- _src/mkl/core.cljs
+++ _src/mkl/core.cljs
@@ -16,7 +16,7 @@
   (let [path   (s/split link #"/")
         slug   (last path)
         title  (s/lower-case slug)
-        target (str "/p/" title ".html")]
+        target (str "/posts/" title ".html")]
     target))
 
 (defn add-find-link! []
