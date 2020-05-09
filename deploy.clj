308a79e3975533c129479e30c2957d569e6d38bb
Martin Klepsch <martinklepsch@googlemail.com>
2020-05-10 00:17:33 +0200

    update s3 deploy

--- deploy.clj
+++ deploy.clj
@@ -1,12 +1,14 @@
 (require '[confetti.s3-deploy :as s3]
          '[clojure.java.io :as io])
 
-(s3/sync!
-  {:access-key (System/getenv "AWS_ACCESS_KEY")
-   :secret-key (System/getenv "AWS_SECRET_KEY")}
-  (System/getenv "S3_BUCKET_NAME")
-  (s3/dir->file-maps (io/file "_site"))
-  {:dry-run? true
-   :report-fn (fn [{:keys [s3-key op]}]
-                (println op s3-key))})
+(defn sync! []
+  (s3/sync!
+    {:access-key (System/getenv "AWS_ACCESS_KEY")
+     :secret-key (System/getenv "AWS_SECRET_KEY")}
+    (System/getenv "S3_BUCKET_NAME")
+    (s3/dir->file-maps (io/file "_site"))
+    {:dry-run? true
+     :report-fn (fn [{:keys [s3-key op]}]
+                  #_(println op s3-key))}))
 
+(prn (sync!))
