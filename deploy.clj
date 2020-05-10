74ed2322a091bf2499da3e853a460df9124b40fc
Martin Klepsch <martinklepsch@googlemail.com>
2020-05-10 14:30:07 +0200

    update deploy script to create cloudfront invalidation

--- deploy.clj
+++ deploy.clj
@@ -1,14 +1,22 @@
 (require '[confetti.s3-deploy :as s3]
          '[clojure.java.io :as io])
 
+(def creds
+  {:access-key (System/getenv "AWS_ACCESS_KEY")
+   :secret-key (System/getenv "AWS_SECRET_KEY")})
+
 (defn sync! []
   (s3/sync!
-    {:access-key (System/getenv "AWS_ACCESS_KEY")
-     :secret-key (System/getenv "AWS_SECRET_KEY")}
+    creds
     (System/getenv "S3_BUCKET_NAME")
     (s3/dir->file-maps (io/file "_site"))
     {:dry-run? false
      :report-fn (fn [{:keys [s3-key op]}]
                   #_(println op s3-key))}))
 
-(prn (sync!))
+(let [sync-results (sync!)
+      inv-paths (->> (mapcat sync-results [:uploaded :updated :deleted])
+                     (map #(str "/" %)))]
+  (when (seq inv-paths)
+    (s3/cloudfront-invalidate! creds (System/getenv "CLOUDFRONT_ID") inv-paths))
+  (prn (assoc sync-results :unchanged :hidden)))
