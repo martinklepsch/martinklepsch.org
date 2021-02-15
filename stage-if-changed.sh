4878fee2eb8c5b9d056b4f95f9e2a73196dee3fe
Martin Klepsch <martinklepsch@googlemail.com>
2021-02-15 21:24:52 +0200

    fix stage-if-changed.sh to stage untracked files in target dir

--- stage-if-changed.sh
+++ stage-if-changed.sh
@@ -7,7 +7,10 @@ dir=$1
 # Atom feeds currently get a new timestamp whenever they are regenerated
 # causing them to trigger a redeploy when really no data changed
 if [[ $(git diff $dir | filterdiff -p 1 -x '_site/atom*.xml') ]]; then
-    echo "Staging files in $dir"
+    echo "Modified files in $dir"
+    git add $dir
+elif [[ $(git ls-files --other --directory --exclude-standard _site/) ]]; then
+    echo "Untracked files in $dir"
     git add $dir
 else
     echo "No changes in $dir"
